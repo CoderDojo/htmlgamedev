38c9823103e27ea2be8da989038de22ed833494b
Tim White <tim.white@ieee.org>
2013-09-29 18:25:52 +0100

    Initial Commit

---
+++ JuggleBalls/Week2/BallObject.js
@@ -0,0 +1,18 @@
+function BallObject(ball) {
+	var ballDiv = ball;
+	var curXpos = 0;
+	var curYpos = 0;
+	
+	this.moveHorizontal = moveHorizontal;
+	this.moveVertical = moveVertical;
+	
+	function moveHorizontal( pixels ) {
+		curXpos = curXpos + pixels;
+		ballDiv.css("left", curXpos+"px");
+	}
+	
+	function moveVertical(pixels) {
+		curYpos = curYpos + pixels;
+		ballDiv.css("top",curYpos+"px");
+	}
+}
---
+++ JuggleBalls/Week2/TurtleBall.js
@@ -0,0 +1,19 @@
+var ball;
+$(document).ready(function() {
+	ball = new BallObject($("#ballDiv"));
+	
+	$("#btnUp").click(function() {
+		ball.moveVertical(-10); 
+	});
+	$("#btnDown").click(function() { 
+		ball.moveVertical(10);
+	});
+	
+	$("#btnLeft").click(function() {
+		ball.moveHorizontal(-10);
+	});
+	
+	$("#btnRight").click(function() {
+		ball.moveHorizontal(10);
+	});
+});
